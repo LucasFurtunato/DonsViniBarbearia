e0b995c8664f7d91f12fd1faf2325e8c855348c3
Lucas Furtunato <lucasfurtunato11@gmail.com>
2024-09-01 19:57:21 -0300

    newIndex

--- index.php
+++ index.php
@@ -1,191 +1,181 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-<?php 
-  session_start();
-  
-  if (isset($_SESSION["cliente"])) {
-    $cliente = $_SESSION["cliente"];
-}
-
-include '.php/repositorio/conexao.php';
-?>
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="COLOCAR ALGUMA DESCRIÇÃO AQUI">
-    <meta name="keywords" content="Barbearia, Salão Masculino, Cabelo, Barba, Cuidados Pessoais">
-    <link rel="stylesheet" href="css/styles.css">
-    <title>Don's Vini - Barbearia </title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+    <title>Home</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
 </head>
 <body>
-
-    <nav class="navbar">
-        <div class="logo">
-            <h1>Don's Vini</h1>
-        </div>
-        <div class="menu">
-            <a href="index.php">Início</a>
-            <a href="#content-sobre">Sobre Nós</a>
-            <a href="#content-servicos">Serviços</a>
-            <a href="#gallery-all">Galeria</a>
-            <a href="#contato">Contato</a>
-            <?php
-            if (isset($_SESSION["cliente"])) 
-            { ?>
-                <a id="botao" href="#"><?php echo $cliente; ?></a>
-                <a id="botao" href=".php/controlador/logout.php">Sair</a>
-            <?php } 
-            else{ ?>
-                <a id="botao" href="login-cadastro.php">Entrar</a>
-            <?php } ?>
-
-            
-        </div>
-    </nav>
-
-    <header class="header-content">
-
-        <section class="call-to-action"> 
-            
-            <div class="container">
-                <h1>Don's Vini</h1>
-                <h2>Barbearias</h2>
-                <a href="#" class="button">Agende já</a>
-              </div>
-
-        </section>
-
+    
+    <header>
+        <nav>
+            <img src="img/logo.png" alt="Don' Vini logo" class="logo">
+            <div class="mobile-menu">
+                <div class="line1"></div>
+                <div class="line2"></div>
+                <div class="line3"></div>
+            </div>
+            <ul class="nav-list">
+                <li><a href="#">Início</a></li>
+                <li><a href="#about">Sobre</a></li>
+                <li><a href="#services">Serviços</a></li>
+                <li><a href="#gallery">Galeria</a></li>
+                <li><a href="#contact">Contato</a></li>
+                <li><a href="#" class="login-button">Entrar</a></li>
+            </ul>
+        </nav>
     </header>
+    <main>
+        <div class="background-container">
+            <h1>Don' Vini</h1>
+            <h2>Barbearias</h2>
+            <a href="#" class="button">Agende já</a>
+        </div>
 
-    <main id="main-content">
+        <section id="about">
 
-        <section id="content-sobre">
-
-            <div class="sobre-text">
-                <h2>Quem Somos?</h2>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus numquam reiciendis necessitatibus earum modi. Quisquam pariatur voluptas quas cum obcaecati laborum consequuntur fugiat quasi tempore iste, nobis neque quia quos.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus numquam reiciendis necessitatibus earum modi. Quisquam pariatur voluptas quas cum obcaecati laborum consequuntur fugiat quasi tempore iste, nobis neque quia quos.</p>
-                <a href="#" class="button">Inscreva-se</a>
-            </div>
-
-        </section>
-
-        <section id="content-servicos">
-
-                <h3 class="p-above">Conheça Nossos</h3>
-                <h2>Serviços</h2>
-        
-            <div class="servicos">
-                <div class="card">
-                    <img src="https://picsum.photos/64/64" alt="">
-                    <div class="card-text">
-                        <h3>SERVIÇO AQUI</h3>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident minus magnam sint, modi molestiae vitae dolorum! Culpa ullam esse magni impedit quaerat accusantium voluptatum minus, dolorum corrupti! Quo, consequatur eligendi.</p>
-                    </div>
-                    <div class="texto-oculto">
-                        <h3>Outro conteúdo qualquer</h3>
-                        <p>Dorem ipsum dolor sit amet consectetur adipisicing elit. Provident minus magnam sint, modi molestiae vitae dolorum! Culpa ullam esse magni impedit quaerat accusantium voluptatum minus, dolorum corrupti! Quo, consequatur eligendi.</p>
-                    </div>
-                </div>
-                <div class="card">
-                    <img src="https://picsum.photos/64/64" alt="">
-                    <div class="card-text">
-                        <h3>SERVIÇO AQUI</h3>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident minus magnam sint, modi molestiae vitae dolorum! Culpa ullam esse magni impedit quaerat accusantium voluptatum minus, dolorum corrupti! Quo, consequatur eligendi.</p>
-                    </div>
-                    <div class="texto-oculto">
-                        <h3>Outro conteúdo qualquer</h3>
-                        <p>Dorem ipsum dolor sit amet consectetur adipisicing elit. Provident minus magnam sint, modi molestiae vitae dolorum! Culpa ullam esse magni impedit quaerat accusantium voluptatum minus, dolorum corrupti! Quo, consequatur eligendi.</p>
-                    </div>
-                </div>
-                <div class="card">
-                    <img src="https://picsum.photos/64/64" alt="">
-                    <div class="card-text">
-                        <h3>SERVIÇO AQUI</h3>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident minus magnam sint, modi molestiae vitae dolorum! Culpa ullam esse magni impedit quaerat accusantium voluptatum minus, dolorum corrupti! Quo, consequatur eligendi.</p>
-                    </div>
-                    <div class="texto-oculto">
-                        <h3>Outro conteúdo qualquer</h3>
-                        <p>Dorem ipsum dolor sit amet consectetur adipisicing elit. Provident minus magnam sint, modi molestiae vitae dolorum! Culpa ullam esse magni impedit quaerat accusantium voluptatum minus, dolorum corrupti! Quo, consequatur eligendi.</p>
-                    </div>
-                </div>
-            </div>
+            <h2>Quem Somos?</h2>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem ut debitis doloremque provident eaque aspernatur cumque nihil obcaecati tempora excepturi. Magnam labore fuga consectetur eum ducimus quisquam temporibus, dolor harum?Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem ut debitis doloremque provident eaque aspernatur cumque nihil obcaecati tempora excepturi. Magnam labore fuga consectetur eum ducimus quisquam temporibus, dolor harum.</p>
+            <a href="#" class="button-generic">Inscreva-se</a>
 
         </section>
 
-        <section id="gallery-all">
+        <section id="services">
             
-            <h3 class="p-above">Veja Nossa</h3>
-            <h2>Galeria</h2>
-        <div class="">
-           <div class="gallery">
-            <div class="gallery-container">
-                <img class="gallery-item gallery-item-1" src="img/1.jpg" alt="" data-index="1">
-                <img class="gallery-item gallery-item-2" src="img/2.jpg" alt="" data-index="2">
-                <img class="gallery-item gallery-item-3" src="img/3.jpg" alt="" data-index="3">
-                <img class="gallery-item gallery-item-4" src="img/4.jpg" alt="" data-index="4">
-                <img class="gallery-item gallery-item-5" src="img/5.jpg" alt="" data-index="5">
+            <h3>Conheça Nossos</h3>
+            <h2>Serviços</h2>
+
+            <div class="services-container">
+
+                <div class="card">
+                    <div class="card-content">
+                        <img src="img/tesoura_e_pente.png" alt="Tesoura">
+                        <h3>Cabelo</h3>
+                        <div class="line"></div>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit, voluptatibus explicabo deserunt.</p>
+                        <div class="hidden-text">
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum quidem maxime voluptatum reprehenderit? At aspernatur aut facilis obcaecati nulla sunt modi consequuntur tempore perspiciatis hic placeat, consequatur ea ducimus magnam?</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card-content">
+                        <img src="img/navalha.png" alt="Navalha">
+                        <h3>Barba</h3>
+                        <div class="line sec-line"></div>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit, voluptatibus explicabo deserunt.</p>
+                        <div class="hidden-text">
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum quidem maxime voluptatum reprehenderit? At aspernatur aut facilis obcaecati nulla sunt modi consequuntur tempore perspiciatis hic placeat, consequatur ea ducimus magnam?</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card-content">
+                        <img src="img/pincel.png" alt="Pincel">
+                        <h3>Cuidados</h3>
+                        <div class="line"></div>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit, voluptatibus explicabo deserunt.</p>
+                        <div class="hidden-text">
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum quidem maxime voluptatum reprehenderit? At aspernatur aut facilis obcaecati nulla sunt modi consequuntur tempore perspiciatis hic placeat, consequatur ea ducimus magnam?</p>
+                        </div>
+                    </div>
+                </div>
+
             </div>
-            <div class="gallery-controls"></div>
-           </div>
-        </div>   
+
+        </section>
+
+        <section id="gallery">
+
+            <h3>Veja Nossa</h3>
+            <h2>Galeria</h2>
+
+            <div class="gallery-content">
+                <div class="wrapper">
+                    <i id="left" class="fa-solid fa-angle-left"></i>
+                    <div class="carousel">
+                        <img src="img/image1.jpg" alt="" draggable="false">
+                        <img src="img/image2.jpg" alt="" draggable="false">
+                        <img src="img/image3.jpg" alt="" draggable="false">
+                        <img src="img/image4.jpg" alt="" draggable="false">
+                        <img src="img/image5.jpg" alt="" draggable="false">
+                    </div>
+                    <i id="right" class="fa-solid fa-angle-right"></i>
+                </div>
+            </div>
         </section>
 
         <div class="striped-line"></div>
 
-        <section id="contato">
+        <section id="contact">
 
-            <h3 class="p-above">Fale</h3>
+            <h3>Fale</h3>
             <h2>Conosco</h2>
 
-            <div class="contato-area">
-
-               <div class="contato-img">
-                    <img src="img/barber.jpeg" alt="" class="barbeiro">
-               </div>
-               <div class="contato-info">
-                    <p class="italico">Nos Contate</p>
-                    <h4>Nossos Contatos</h4>
-                    <div class="small-section">
-                        <img src="img/icons8-telefone-50.png" alt="">
+            <div class="contact-container">
+                <div class="square square1">
+                    <img src="img/barber-contact.jpeg" alt="Ilustração de barbeiro">
+                </div>
+                <div class="square square2">
+                    <p id="p-italic">Nos contate</p>
+                    <p id="p-top">Nossos Contatos</p>
+                    <div class="icons">
+                        <i class="fa-solid fa-phone-volume"></i>
                         <p>21 0000-0000</p>
                     </div>
-                    <div class="small-section">
-                        <img src="img/icons8-email-50.png" alt="">
-                        <p>email_teste@gmail.com</p>
+                    <div class="icons">
+                        <i class="fa-solid fa-envelope"></i>
+                        <p>teste123@gmail.com</p>
                     </div>
-                    <div class="small-section">
-                        <img src="img/icons8-smartphone-50.png" alt="">
-                        <p>11 00000-0000</p>
+                    <div class="icons">
+                        <i class="fa-brands fa-whatsapp"></i>
+                        <p>21 0000-0000</p>
                     </div>
-                    <div class="small-section">
-                        <img src="img/icons8-localização-50.png" alt="">
-                        <p>Rua NomeRua, 000, Guarulhos, SP. Das 13h às 20h</p>
+                    <div class="icons">
+                        <i class="fa-solid fa-location-dot"></i>
+                        <p>Unidade 1: Rua Avenida Sei Lá Oque, 0000-000</p>
                     </div>
-                    <p class="italico">Nos siga em</p>
-                    <div class="redes-sociais">
-                        <a href=""><img src="img/icons8-instagram-50.png" alt=""></a>
-                        <a href=""><img src="img/icons8-facebook-novo-50.png" alt=""></a>
-                        <a href=""><img src="img/icons8-reproduzir-youtube-50.png" alt="" ></a>
+                    <div class="icons">
+                        <i class="fa-solid fa-location-dot"></i>
+                        <p>Unidade 2: Rua Avenida Sei Lá Oque, 0000-000</p>
                     </div>
-               </div>
-
+                    <div class="icons-social-media">
+                        <a href=""><i class="fa-brands fa-instagram fa-2x"></i></a>
+                        <a href=""><i class="fa-brands fa-facebook fa-2x"></i></a>
+                        <a href=""><i class="fa-brands fa-youtube fa-2x"></i></a>
+                    </div>
+                </div>
             </div>
         
         </section>
 
-        <section id="mapa">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d234337.73703207538!2d-46.557856270466!3d-23.405317633732594!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sInstituto%20Federal%20de%20Educa%C3%A7%C3%A3o%2C%20Ci%C3%AAncia%20e%20Tecnologia%20de%20S%C3%A3o%20Paulo!5e0!3m2!1spt-BR!2sbr!4v1716739406273!5m2!1spt-BR!2sbr" width="100%" height="250" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        <section class="map-container">
+
+            <div class="maps">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d234337.73703207538!2d-46.557856270466!3d-23.405317633732594!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sInstituto%20Federal%20de%20Educa%C3%A7%C3%A3o%2C%20Ci%C3%AAncia%20e%20Tecnologia%20de%20S%C3%A3o%20Paulo!5e0!3m2!1spt-BR!2sbr!4v1716739406273!5m2!1spt-BR!2sbr" width="400" height="250" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                <p>Unidade 1</p>
+            </div>
+            <div class="maps">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d234337.73703207538!2d-46.557856270466!3d-23.405317633732594!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sInstituto%20Federal%20de%20Educa%C3%A7%C3%A3o%2C%20Ci%C3%AAncia%20e%20Tecnologia%20de%20S%C3%A3o%20Paulo!5e0!3m2!1spt-BR!2sbr!4v1716739406273!5m2!1spt-BR!2sbr" width="400" height="250" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                <p>Unidade 2</p>
+            </div>
+
         </section>
-        
+
     </main>
 
-    <footer class="footer">
+    <footer>
         <div class="footer-content">
-            <p>&copy; 2024 Barbearias Don's Vini. Todos os direitos reservados.</p>
+            <p>&copy; 2024 Barbearias Don' Vini. Todos os direitos reservados.</p>
             <p>Por: RootingTI</p>
         </div>
     </footer>
 
-    <script src="js/script.js"></script>
-</body>
+    <script src="js/mobile-navbar.js"></script>
+    <script src="js/gallery.js"></script>
+</body> 
+
+
 </html>
