61e7da715126165f8dc8989f9654e5bae7a76d0c
LucasFurtunato <95308325+LucasFurtunato@users.noreply.github.com>
2024-09-18 14:08:15 -0300

    Add files via upload

---
+++ donvinibarbearia.sql
@@ -0,0 +1,163 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 18-Set-2024 às 19:07
+-- Versão do servidor: 10.4.24-MariaDB
+-- versão do PHP: 8.0.19
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `donvinibarbearia`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `agendamentos`
+--
+
+CREATE TABLE `agendamentos` (
+  `SVC_ID` int(10) NOT NULL,
+  `UNIDADE` int(2) NOT NULL,
+  `SERVICO` int(4) NOT NULL,
+  `PRECO` int(255) NOT NULL,
+  `DIA` date NOT NULL,
+  `HORARIO` time NOT NULL,
+  `FK_EMAIL` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `cliente`
+--
+
+CREATE TABLE `cliente` (
+  `NOME` varchar(255) NOT NULL,
+  `EMAIL` varchar(255) NOT NULL,
+  `SENHA` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `cliente`
+--
+
+INSERT INTO `cliente` (`NOME`, `EMAIL`, `SENHA`) VALUES
+('samuel', 's@gmail.com', '$2y$10$5xibOk2CwUS86BClfhdCpuGsuCi/IHamzfqxwAaSr.suoMDpwpM8W');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `funcionario`
+--
+
+CREATE TABLE `funcionario` (
+  `CODIGO` varchar(10) NOT NULL,
+  `NOME` varchar(250) NOT NULL,
+  `EMAIL` varchar(250) NOT NULL,
+  `UNIDADE` int(2) NOT NULL,
+  `SENHA` varchar(250) NOT NULL,
+  `CONFIRMARSENHA` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `galeria`
+--
+
+CREATE TABLE `galeria` (
+  `ID` int(100) NOT NULL,
+  `LOCALIZACAO` int(3) NOT NULL,
+  `IMAGEM` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `gerente`
+--
+
+CREATE TABLE `gerente` (
+  `CODIGO` varchar(10) NOT NULL,
+  `EMAIL` varchar(250) NOT NULL,
+  `SENHA` varchar(250) NOT NULL,
+  `NOME` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `agendamentos`
+--
+ALTER TABLE `agendamentos`
+  ADD PRIMARY KEY (`SVC_ID`),
+  ADD KEY `FK_EMAIL` (`FK_EMAIL`);
+
+--
+-- Índices para tabela `cliente`
+--
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`EMAIL`);
+
+--
+-- Índices para tabela `funcionario`
+--
+ALTER TABLE `funcionario`
+  ADD PRIMARY KEY (`CODIGO`);
+
+--
+-- Índices para tabela `galeria`
+--
+ALTER TABLE `galeria`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Índices para tabela `gerente`
+--
+ALTER TABLE `gerente`
+  ADD PRIMARY KEY (`CODIGO`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `agendamentos`
+--
+ALTER TABLE `agendamentos`
+  MODIFY `SVC_ID` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `galeria`
+--
+ALTER TABLE `galeria`
+  MODIFY `ID` int(100) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restrições para despejos de tabelas
+--
+
+--
+-- Limitadores para a tabela `agendamentos`
+--
+ALTER TABLE `agendamentos`
+  ADD CONSTRAINT `agendamentos_ibfk_1` FOREIGN KEY (`FK_EMAIL`) REFERENCES `cliente` (`EMAIL`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
