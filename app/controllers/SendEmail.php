a08a5afed6ceae84edc1b83e2baa3854cac972ed
root <root@myhost.myhost>
2024-12-07 12:17:18 -0300

    Arrumando o texto do email

--- app/controllers/SendEmail.php
+++ app/controllers/SendEmail.php
@@ -6,8 +6,11 @@ function enviarEmailConfirmacao($email, $nome, $token) {
         $to = $email;
         $subject = "Comfirme seu Email em Don'Vini Barbearia";
         $link = "localhost/donvinibarbearia/public/confirmar_email.html?token=" . urlencode($token);
-        $message = "Olá,$nome<br><br>Por favor, clique no link abaixo para confirmar seu e-mail:<br><br>
-                <a href='$link'>$link</a><br><br>Se você não solicitou isso, ignore este e-mail.";
+        $message = "Olá, $nome. Por favor, clique no link abaixo para confirmar seu e-mail:
+
+$link
+
+Se você não solicitou isso, ignore este e-mail.";
         
         $send = \mail($to, $subject, $message);
         
@@ -25,8 +28,11 @@ function enviarCodRecuperacaoSenha($email, $nome, $token) {
     try {
         $to = $email;
         $subject = "Codigo de recuperação de senha em Don'Vini Barbearia";
-        $message = "Olá,<br><br>Por favor, use o código abaixo para recuperar sua senha:<br><br>
-                    <h2>$token</h2><br><br>Se você não solicitou isso, ignore este e-mail.";
+        $message = "Olá,$nome. Por favor, use o código abaixo para recuperar sua senha:
+
+$token
+
+Se você não solicitou isso, ignore este e-mail.";
         
         $send = \mail($to, $subject, $message);
         
