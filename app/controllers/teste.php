1d70e9e700b607944c1f541593a93f2ed29438ee
root <root@myhost.myhost>
2024-12-07 11:30:56 -0300

    testando novamente o envio de email clear

--- app/controllers/teste.php
+++ app/controllers/teste.php
@@ -4,14 +4,19 @@ namespace app\controllers;
 
 require_once  dirname( __DIR__ , 1).'/config.php';
 
-$cliente = new CtrlCliente();
+$to = "lucasfurtunato11@gmail.com";
+$subject = "Olá, apenas um teste de envio de email, não se apavore, provavelmente coloquei o email errado";
 
-if ($cliente->eviandoEmail()) {
-    echo "<br> Sucesso ao enviar email: " . $cliente->eviandoEmail();
-} else {
-    echo "<br> Erro ao enviar email: " . $cliente->eviandoEmail();
-}
+$headers = array(
+    "MIME-Version" => "1.0",
+    "Content-Type" => "text/html;charset=UTF-8",
+    "From" => "lucasfurtunato11@gmail.com",
+    "Reply-To" => "lucasfurtunato11@gmail.com"
+);
 
+$message = "oi, isso é apenas um teste";
 
+$send = mail($to, $subject, $message, $headers);
 
+echo ($send ? "Email enviado" : "Houve algum erro ao enviar");
 ?>
